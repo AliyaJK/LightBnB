f4542b54d910cde7f1b2a9c181b4c72a21424c08
AliyaJK <kanani.aliya@gmail.com>
2023-06-18 01:56:45 +0000

    bug fixes

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -79,7 +79,7 @@ const addUser = function(user) {
 const getAllReservations = function(guest_id, limit = 10) {
   return pool
     .query(
-      `SELECT reservations.*, properties.*, AVG(property_reviews.rating) as avarage_rating
+      `SELECT reservations.*, properties.*, properties.cost_per_night, reservations.start_date, AVG(property_reviews.rating) AS average_rating
       FROM reservations
       JOIN properties ON reservations.property_id = properties.id
       JOIN property_reviews ON properties.id = property_reviews.property_id
